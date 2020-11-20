6d72768fcbccd2752de05165df8eb3d6768d6b91
Bohdan Stetsiuk <rudebs@outlook.com>
2020-11-20 11:45:06 +0200

    Refactoring

--- src/api/PokeApi.ts
+++ src/api/PokeApi.ts
@@ -4,7 +4,7 @@ import {
 
 export class PokeApi {
     private static api: string = "https://pokeapi.co/api/v2"
-    private static count: number = 0
+    private static pokemonsCount: number = 0
 
     public static async getPokemon(id: string | number) {
         const pokemon = await fetch(`${this.api}/pokemon/${id}`);
@@ -21,16 +21,16 @@ export class PokeApi {
     }
 
     public static async searchPokemons(query: string, offset: number, limit: number): Promise<IPokemonList> {
-        if (!this.count) {
-            const pokemonsCount = await (
+        if (!this.pokemonsCount) {
+            const pokemonsInfo = await (
                 await fetch(`${this.api}/pokemon?limit=1`)
             ).json() as IPokemonList;
 
-            this.count = pokemonsCount.count;
+            this.pokemonsCount = pokemonsInfo.count;
         }
 
         const pokemonList = await (
-            await fetch(`${this.api}/pokemon?limit=${this.count}&offset=0`)
+            await fetch(`${this.api}/pokemon?limit=${this.pokemonsCount}&offset=0`)
         ).json() as IPokemonList;
         
         const result = pokemonList.results
